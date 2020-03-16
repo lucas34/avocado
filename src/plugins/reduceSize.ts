141e5baffcadc60965c3583dd4f2cbfd6bf6f690
Lucas Nelaupe <lucas.nelaupe@grabtaxi.com>
2020-03-16 10:55:18 +0800

    Resize large image

---
+++ src/plugins/reduceSize.ts
@@ -0,0 +1,34 @@
+import { JsApi } from '../lib/jsapi';
+import { Plugin } from './_types';
+
+const widthAttr = `android:width`;
+const heightAttr = `android:height`;
+
+export function fn(item: JsApi) {
+  if (!item.isElem() || !item.attrs) {
+    return item;
+  }
+  if (item.isElem('vector') && item.hasAttr(widthAttr) && item.hasAttr(heightAttr)) {
+
+    const width = +item.attr(widthAttr).value.replace("dp", "")
+    const height = +item.attr(heightAttr).value.replace("dp", "")
+
+    if (width > 200) {
+      const finalHeight = Math.round(200 * height / width)
+
+      item.attr(widthAttr).value = '200dp'
+      item.attr(heightAttr).value = `${finalHeight}dp`
+    }
+  }
+
+  return item;
+}
+
+
+export const fixHeightPlugin: Plugin<undefined> = {
+  type: 'perItemReverse',
+  active: true,
+  description: 'removes empty groups',
+  params: undefined,
+  fn,
+};
